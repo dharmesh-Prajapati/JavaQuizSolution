da8301991b00f6c6a05172882ea7f2a4d907571e
Dharmesh <dharmeshvistula@gmail.com>
2020-06-01 14:43:13 +0200

    repeated character not in one string

---
+++ QuizRepeatedWord.java
@@ -0,0 +1,39 @@
+package Quiz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizRepeatedWord {
+
+	static List<String> alternatingCharacters(String s) {
+		List<String> repeatedString = new ArrayList<String>();
+		
+		char charArr [] = s.toCharArray();
+		
+		for(int i = 0; i< charArr.length;i++) {
+			repeatedString.add(Character.toString(charArr[i]));
+			
+		}
+		String str ="";
+		List<String> notRepeatedString = new ArrayList<String>();
+		
+		for(int i = 0; i< repeatedString.size();i++) {
+			if(str.contains(repeatedString.get(i))) {
+				notRepeatedString.add(str);
+				str=repeatedString.get(i);
+			}else {
+				str+=repeatedString.get(i);
+			}
+		}
+		notRepeatedString.add(str);
+		return notRepeatedString;
+	}
+	
+	public static void main(String[] args) {
+		List<String> repeatering = alternatingCharacters("asdsasedsawerefdfgdfdfd");
+		for(String str : repeatering) {
+			System.out.println(str);
+		}
+	}
+
+}
